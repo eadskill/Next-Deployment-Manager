ca7b002d6b22ade6f4b55c16b0577af73544ec7d
Diego Souza <dev@eadskill.com.br>
2019-05-08 14:15:16 -0300

    Feat: Client controller and list clients next

--- app/Controllers/Http/ClientController.js
+++ app/Controllers/Http/ClientController.js
@@ -1,8 +1,14 @@
 "use strict";
 
+const Client = use("App/Models/Instance");
+
 class ClientController {
-	async index({ view }) {
-		return view.render("frontend.clients.index");
+	async index({ view, response }) {
+		const clients = await Client.all();
+
+		return view.render("frontend.clients.index", {
+			clients: clients.toJSON()
+		});
 	}
 }
 
--- app/Models/Instance.js
+++ app/Models/Instance.js
@@ -1,9 +1,20 @@
-'use strict'
+"use strict";
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
+const moment = require("moment");
 
 class Instance extends Model {
+	static get computed() {
+		return ["created_at_format", "updated_at_format"];
+	}
+
+	getCreatedAtFormat({ created_at }) {
+		return moment(created_at).format("DD/MM/YYYY HH:mm");
+	}
+
+	getUpdatedAtFormat({ updated_at }) {
+		return moment(updated_at).format("DD/MM/YYYY HH:mm");
+	}
 }
 
-module.exports = Instance
+module.exports = Instance;
