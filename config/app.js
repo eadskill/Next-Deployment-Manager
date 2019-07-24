a46c242cac7a44c61ce691d408011ebc83f6466a
Diego Souza <dev@eadskill.com.br>
2019-07-24 13:37:38 -0300

    Feat: Trust proxy define true

--- config/app.js
+++ config/app.js
@@ -53,7 +53,7 @@ module.exports = {
     | values. Read documentation for that.
     |
     */
-		trustProxy: false,
+		trustProxy: true,
 
 		/*
     |--------------------------------------------------------------------------
